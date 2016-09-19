cf88f7316590f9ae1882db52b67bb2b32aebcfe6
ander <ander@ANDERS-ACER>
2016-09-19 13:12:47 +0200

    Initial commit

---
+++ httpstart/Echo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpstart
+{
+    class Echo
+    {
+        private TcpClient connectionseet;
+
+        public Echo(TcpClient connectionseet)
+        {
+            this.connectionseet = connectionseet;
+        }
+
+        internal void Message()
+        {
+            Stream connectionStream = connectionseet.GetStream();
+
+
+            StreamReader sReader = new StreamReader(connectionStream);
+            StreamWriter sWriter = new StreamWriter(connectionStream);
+            sWriter.AutoFlush = true;
+            string message = "q";
+
+            while (message.ToLower() != "stop")
+            {
+
+                message = sReader.ReadLine();
+
+
+                if (message.StartsWith("GET") && message.EndsWith("HTTP/1.1") && message != "GETHTTP/1.1" &&
+                    message != "GET HTTP/1.1")
+                {
+                    try
+                    {
+                        string[] messeges = new string[3];
+
+                        messeges = message.Split(' ');
+
+                        message = messeges[2];
+
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+
+                        sWriter.WriteLine("bad reques");
+                    }
+                    catch (Exception x)
+                    {
+                        throw x;
+                    }
+
+                }
+
+                else
+                {
+                    sWriter.WriteLine(message.ToUpper() + " Nu Er Jeg Ekoo MHUHAHAH");
+                }
+
+                Console.WriteLine(message);
+
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
--- httpstart/Program.cs
+++ httpstart/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace httpstart
 {
@@ -10,6 +15,33 @@ namespace httpstart
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TcpListener myServer = new TcpListener(IPAddress.Any, 6789);
+                myServer.Start();
+
+                while (true)
+                {
+                    TcpClient myServerTcpconnection = myServer.AcceptTcpClient();
+                    Console.WriteLine("Server started");
+                    Echo service = new Echo(myServerTcpconnection);
+                    Thread myThread = new Thread(service.Message);
+                    myThread.Start();
+                    myServer.Stop();
+
+
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadKey();
+            }
+
+
         }
     }
 }
